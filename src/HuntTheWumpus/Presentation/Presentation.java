253af2389a66e6ee5aec4ab0682e0aed4605665e
Paul Blair <ciriwe@phobot.net>
2012-05-31 17:24:42 -0400

    Removed unused methods from Presentation interface

--- src/HuntTheWumpus/Presentation/Presentation.java
+++ src/HuntTheWumpus/Presentation/Presentation.java
@@ -1,14 +1,5 @@
 package HuntTheWumpus.Presentation;
 
 public interface Presentation {
-
   void outputResponse(ResponseModel responseModel);
-
-  void printUnknownCommand(String command);
-
-  void printShotArrow();
-
-  void printNoArrows();
-
-  void printCannotMove(String direction);
 }
